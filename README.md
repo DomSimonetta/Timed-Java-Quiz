62233edb8f8a999e95dff2aa8e40fe134dc0092c
DomSimonetta <Guitardudedom@gmail.com>
2023-12-12 19:36:26 -0500

    finishing touches, added questions, and functionality

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Timed-Java-Quiz
-A timed java quiz for module 4 to text my knowledge on javascript.
+A timed javascript and coding quiz for module 4 to test knowledge on javascript.
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,106 @@
-var question = document.querySelector(".question");
-var correct = document.querySelector(".correct");
-var incorrect = document.querySelector(".incorrect");
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
+var start = document.getElementById("start-button");
+var questionBox = document.getElementById('question-box');
+var questionElement = document.getElementById('questions');
+var answerButtonsElement = document.getElementById('answer-buttons');
+var shuffledQuestions, currentQuestionIndex
+var timeEl = document.querySelector(".time");
+var secondsLeft = 60;
 
-var scoreboard;
-var isWin = false;
-var timer;
-var timerCount = 60;
+start.addEventListener('click', startGame);
+
 
 function startGame() {
-    timerCount = 60;
+console.log("start");
+shuffledQuestions = questions.sort(() => Math.random() - .5);
+currentQuestionIndex = 0;
+setNextQuestion();
+setTime();
 }
 
-function startTimer() {
-    timer - setInterval(function() {
-        timerCount--;
-        timerElement.textContent = timerCount;
-        if (timerCount === 0) {
-            clearInterval(timer);
-            sendMessage("game over");
-        }
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft;
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
     }, 1000);
+  }
+
+
+function setNextQuestion() {
+    
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+
 }
-startTimer();
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innertext = answer.text;
+        button.classList.add('button');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    })
+}
+
+
+
+ function setStatusClass(element, correct) {
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');   
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const questions = [
+    {
+        question:"what does HTML stand for",
+        answers: [
+            {text: "Hyper Text Monster Link", correct: false},
+            {text: "Huge Text Markup Link", correct: false},
+            {text: "Hyper Text Markup Link", correct: true},
+            {text: "I dont know", correct: false}
+        ]
+    }
+]
+
+const question = [
+    {
+        question:"what does CSS stand for",
+        answers: [
+            {text: "Cascading Style Sheets", correct: true},
+            {text: "Corresponding Style Sheets", correct: false},
+            {text: "Cascading Sheet Styles", correct: false},
+            {text: "I dont know", correct: false}
+        ]
+    }
+]
+
+
+setTime();
+
+
+
+
+
